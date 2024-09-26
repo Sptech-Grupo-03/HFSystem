a77bf61ee2c84ec53b925f807b1516194505ec2b
sardinh4 <leonardo_sardinha@outlook.com>
2024-09-26 20:11:56 -0300

    declaração de variáveis comentada

--- Desenvolvimento/Sprint 2/Site Institucional/Calculadora/scriptLeo.js
+++ Desenvolvimento/Sprint 2/Site Institucional/Calculadora/scriptLeo.js
@@ -9,50 +9,90 @@ permitindo um melhor controle da disponibilidade de água para as plantas.
 
 */
 
-function exibirComoHFSystemPodeAjudar(){
+function exibirComoHFSystemPodeAjudar() {
+  // CRIAÇÃO DE VARIÁVEIS E ATRIBUIÇÃO DE VALORES
 
-// CRIAÇÃO DE VARIÁVEIS E ATRIBUIÇÃO DE VALORES
+  // TIPO DE PLANTAÇÃO
+  const tipoPlantacao = document.getElementById("input_tipo_plantacao").value;
 
-    const tipoPlantacao = document.getElementById("input_tipo_plantacao").value
-    const qtdCicloAnualProducao = document.getElementById("input_qtd_ciclo_anual_producao").value
-    
-    if(document.getElementById("input_trabalha_com_sistema_irrigacao_true").checked){
-        const trabalhaSistemas = true
-    }else if(document.getElementById("input_trabalha_com_sistema_irrigacao_false").checked){
-        const trabalhaSistemas = false
-    }
+  // QUANTIDADE DE CICLOS ANUAL
+  const qtdCiclosAnualProducao = document.getElementById(
+    "input_qtd_ciclos_anual_producao"
+  ).value;
 
-    if(document.getElementById("input_possui_reservatorio_true").checked){
-        const possuiReservatorio = true
-        const capacidadeReservatorio = document.getElementById("input_capacidade_reservatorio").value
-    }else if(document.getElementById("input_possui_reservatorio_false").checked){
-        const possuiReservatorio = false
-    }
+  // SE SABE CONSUMO DE ÁGUA MENSAL
 
-    const consumoAguaMensal = document.getElementById("input_consumo_agua_mensal").value
+  if (document.getElementById("nao_sei_consumo_agua_mensal").checked) {
+    const consumoAguaMensal = document.getElementById(
+      "nao_sei_consumo_agua_mensal"
+    ).value;
+  } else {
+    // SE NÃO SABE CONSUMO DE ÁGUA MENSAL
+    const consumoAguaMensal = document.getElementById(
+      "input_consumo_agua_mensal"
+    ).value;
+  }
 
-    if(document.getElementById("input_possui_sistema_monitoramento_reservatorio_true").checked){
-        const possuiMonitoramentoReservatorio = true
-        
-        if(document.getElementById("input_tipo_sistema_manual").checked){
-            const tipoMonitoramentoReservatorio = 'manual'
-        }else if(document.getElementById("input_tipo_sistema_automatico").checked){
-            const tipoMonitoramentoReservatorio = 'automático'
+  // SE TRABALHA COM SISTEMAS
+  if (
+    document.getElementById("input_trabalha_com_sistema_irrigacao_true").checked
+  ) {
+    const trabalhaSistemas = true;
+
+    // SE POSSUÍ RESERVATÓRIO
+    if (document.getElementById("input_possui_reservatorio_true").checked) {
+      const possuiReservatorio = true;
+      const capacidadeReservatorio = document.getElementById(
+        "input_capacidade_reservatorio"
+      ).value;
+
+      // SE POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
+      if (
+        document.getElementById(
+          "input_possui_sistema_monitoramento_reservatorio_true"
+        ).checked
+      ) {
+        const possuiMonitoramentoReservatorio = true;
+
+        // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR MANUAL
+        if (document.getElementById("input_tipo_sistema_manual").checked) {
+          const tipoMonitoramentoReservatorio = "manual";
+
+          // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR AUTOMÁTICO
+        } else if (
+          document.getElementById("input_tipo_sistema_automatico").checked
+        ) {
+          const tipoMonitoramentoReservatorio = "automático";
         }
 
-    }else if(document.getElementById("input_possui_sistema_monitoramento_reservatorio_false").checked){
-        const possuiMonitoramentoReservatorio = false
+        // SE NÃO POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
+      } else if (
+        document.getElementById(
+          "input_possui_sistema_monitoramento_reservatorio_false"
+        ).checked
+      ) {
+        const possuiMonitoramentoReservatorio = false;
+      }
+
+      // SE NÃO POSSUÍ RESERVATÓRIO
+    } else if (
+      document.getElementById("input_possui_reservatorio_false").checked
+    ) {
+      const possuiReservatorio = false;
     }
 
-    // lÓGICA
+    // SE NÃO NÃO POSSUI SISTEMA DE IRRIGAÇÃO
+  } else if (
+    document.getElementById("input_trabalha_com_sistema_irrigacao_false")
+      .checked
+  ) {
+    const trabalhaSistemas = false;
+  }
 
-    // SE POSSUÍ TRABALHA COM SISTEMA DE IRRIGAÇÃO
+  // lÓGICA
 
-    if(possuiReservatorio){
-        
-    }
+  // SE O USUÁRIO SABE QUANTIDADE DE CONSUMO DE ÁGUA MENSAL
 
-
-
-
+  if (possuiReservatorio) {
+  }
 }
