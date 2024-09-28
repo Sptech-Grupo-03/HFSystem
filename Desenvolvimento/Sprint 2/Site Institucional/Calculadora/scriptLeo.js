72e0200cae593916a5da7e937d9f590d55825511
RennanSMoura <rennan.moura@sptech.school>
2024-09-27 21:26:11 -0300

    criação de cálculos baseado nas respostas do usuário

--- Desenvolvimento/Sprint 2/Site Institucional/Calculadora/scriptLeo.js
+++ Desenvolvimento/Sprint 2/Site Institucional/Calculadora/scriptLeo.js
@@ -13,75 +13,75 @@ não possuem nenhum tipo de
  */
 
 function exibirComoHFSystemPodeAjudar() {
-    // CRIAÇÃO DE VARIÁVEIS E ATRIBUIÇÃO DE VALORES TIPO DE PLANTAÇÃO
-    const tipoPlantacao = document
-        .getElementById("input_tipo_plantacao")
-        .value
+  // CRIAÇÃO DE VARIÁVEIS E ATRIBUIÇÃO DE VALORES TIPO DE PLANTAÇÃO
+  const tipoPlantacao = document
+      .getElementById("input_tipo_plantacao")
+      .value
 
-    // QUANTIDADE DE CICLOS ANUAL
-    const qtdCiclosAnualProducao = document
-        .getElementById(
-            "input_qtd_ciclos_anual_producao"
-        )
-        .value
+  // QUANTIDADE DE CICLOS ANUAL
+  const qtdCiclosAnualProducao = document
+      .getElementById(
+          "input_qtd_ciclos_anual_producao"
+      )
+      .value
 
-    // SE SABE CONSUMO DE ÁGUA MENSAL
+  // SE SABE CONSUMO DE ÁGUA MENSAL
 
-    if (document.getElementById("nao_sei_consumo_agua_mensal").checked) {
-        const consumoAguaMensal = document
-            .getElementById(
-                "nao_sei_consumo_agua_mensal"
-            )
-            .value
-    } else {
-        // SE NÃO SABE CONSUMO DE ÁGUA MENSAL
-        const consumoAguaMensal = document
-            .getElementById("input_consumo_agua_mensal")
-            .value
-    }
+  if (document.getElementById("nao_sei_consumo_agua_mensal").checked) {
+      const consumoAguaMensal = document
+          .getElementById(
+              "nao_sei_consumo_agua_mensal"
+          )
+          .value
+  } else {
+      // SE NÃO SABE CONSUMO DE ÁGUA MENSAL
+      const consumoAguaMensal = document
+          .getElementById("input_consumo_agua_mensal")
+          .value
+  }
 
-    // SE TRABALHA COM SISTEMAS DE IRRIGAÇÃO
-    if (document.getElementById("input_trabalha_com_sistema_irrigacao_true").checked) {
-        const trabalhaSistemas = true
+  // SE TRABALHA COM SISTEMAS DE IRRIGAÇÃO
+  if (document.getElementById("input_trabalha_com_sistema_irrigacao_true").checked) {
+      const trabalhaSistemas = true
 
-        // SE NÃO NÃO POSSUI SISTEMA DE IRRIGAÇÃO
-    } else if (document.getElementById("input_trabalha_com_sistema_irrigacao_false").checked) {
-        const trabalhaSistemas = false
-    }
+      // SE NÃO NÃO POSSUI SISTEMA DE IRRIGAÇÃO
+  } else if (document.getElementById("input_trabalha_com_sistema_irrigacao_false").checked) {
+      const trabalhaSistemas = false
+  }
 
-    // SE POSSUÍ RESERVATÓRIO
-    if (document.getElementById("input_possui_reservatorio_true").checked) {
-        const possuiReservatorio = true;
-        const capacidadeReservatorio = document
-            .getElementById(
-                "input_capacidade_reservatorio"
-            )
-            .value
+  // SE POSSUÍ RESERVATÓRIO
+  if (document.getElementById("input_possui_reservatorio_true").checked) {
+      const possuiReservatorio = true;
+      const capacidadeReservatorio = document
+          .getElementById(
+              "input_capacidade_reservatorio"
+          )
+          .value
 
-        // SE POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
-        if (document.getElementById("input_possui_sistema_monitoramento_reservatorio_true").checked) {
-            const possuiMonitoramentoReservatorio = true
+      // SE POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
+      if (document.getElementById("input_possui_sistema_monitoramento_reservatorio_true").checked) {
+          const possuiMonitoramentoReservatorio = true
 
-            // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR MANUAL
-            if (document.getElementById("input_tipo_sistema_manual").checked) {
-                const tipoMonitoramentoReservatorio = "manual"
+          // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR MANUAL
+          if (document.getElementById("input_tipo_sistema_manual").checked) {
+              const tipoMonitoramentoReservatorio = "manual"
 
-                // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR AUTOMÁTICO
-            } else if (document.getElementById("input_tipo_sistema_automatico").checked) {
-                const tipoMonitoramentoReservatorio = "automático"
-            }
+              // SE TIPO DO SISTEMA DE MONITORAMENTO DO RESERVATÓRIO FOR AUTOMÁTICO
+          } else if (document.getElementById("input_tipo_sistema_automatico").checked) {
+              const tipoMonitoramentoReservatorio = "automático"
+          }
 
-            // SE NÃO POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
-        } else if (document.getElementById("input_possui_sistema_monitoramento_reservatorio_false").checked) {
-            const possuiMonitoramentoReservatorio = false
-            // fim do código
-        }
+          // SE NÃO POSSUÍ SISTEMA DE MONITORAMENTO DE RESERVATÓRIO
+      } else if (document.getElementById("input_possui_sistema_monitoramento_reservatorio_false").checked) {
+          const possuiMonitoramentoReservatorio = false
+          // fim do código
+      }
 
-        // SE NÃO POSSUÍ RESERVATÓRIO
-    } else if (document.getElementById("input_possui_reservatorio_false").checked) {
-        const possuiReservatorio = false
+      // SE NÃO POSSUÍ RESERVATÓRIO
+  } else if (document.getElementById("input_possui_reservatorio_false").checked) {
+      const possuiReservatorio = false
 
-        //fim do código
-    }
+      //fim do código
+  }
 
 }
