c3e1b098adfd3b10aa0fd1e97cdb9ce0eab87898
sardinh4 <leonardo_sardinha@outlook.com>
2024-10-08 12:07:27 -0300

    dat-acqu-ino

---
+++ Desenvolvimento/Sprint 2/Banco de Dados/hfsystem.sql
@@ -0,0 +1,7 @@
+create database HFSystem;
+
+use HFSystem;
+
+create table HCSR04 (idHCSR04 int primary key auto_increment, distanciaAgua float, dataHoraCapturaSensor date);
+
+select * from HCSR04;
