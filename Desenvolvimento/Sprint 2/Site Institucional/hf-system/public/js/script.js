398ddce388b4ef68d02e08c58af731d58ce60b7d
sardinh4 <leonardo_sardinha@outlook.com>
2024-10-13 00:40:47 -0300

    refatorando o site intitucional e reorganizando as pastas

---
+++ Desenvolvimento/Sprint 2/Site Institucional/hf-system/public/js/script.js
@@ -0,0 +1,21 @@
+function menuSelecionado(menu){
+    
+    document.getElementById('link_home').classList.remove('menu_selecionado')
+    document.getElementById('link_sobre_nos').classList.remove('menu_selecionado')
+    document.getElementById('link_solucoes').classList.remove('menu_selecionado')
+    document.getElementById('link_section_contact').classList.remove('menu_selecionado')
+    document.getElementById('link_resgistrar').classList.remove('menu_selecionado')
+
+
+    document.getElementById(menu).classList.add('menu_selecionado')
+
+}
+
+document.addEventListener("DOMContentLoaded", menuSelecionado('link_home'))
+window.onload = function() {
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+      }
+    
+    window.scrollTo(0, 0);
+  };
