1a9130a06992cea7e2de619d21f0f0be1e4e4d76
RennanSMoura <rennan.moura@sptech.school>
2024-10-16 21:50:32 -0300

    Adição de validacao para login e ajuste de icone na tela de cadastro

--- Desenvolvimento/Sprint 2/Site/hf-system/auth/js/auth.js
+++ Desenvolvimento/Sprint 2/Site/hf-system/auth/js/auth.js
@@ -1,14 +1,19 @@
 function validacaoTamanhoUsuario() {
-    var usuario = document.getElementById('usuario').value
+    var usuario = document.getElementById('input_usuario').value
     var tamanhoMininoUsuario = 6;
     var randomuser = document.getElementById('randomuser');
+    var iconeLogin = document.getElementById('iconeLogin');
     var mensagem = '';
+    
 
+    iconeLogin.style.top = '50%';
     for (var i = usuario.length; i < tamanhoMininoUsuario; i++) {
-        mensagem = `<p>Seu nome de usuário deve conter pelo menos 6 caracteres</p>`;
+        mensagem = `Seu nome de usuário deve conter pelo menos 6 caracteres`;
+        iconeLogin.style.top = '33%';
+    
     }
-        randomuser.innerHTML = mensagem;
-
+    randomuser.innerHTML = mensagem;
+    
 }
 
 function toggleSenhaVisivel() {
@@ -38,5 +43,27 @@ function toggleConfirmacaoSenhaVisivel() {
         confirmacaoSenha.type = 'password';
         toggleIcon.classList.remove('fa-eye-slash');
         toggleIcon.classList.add('fa-eye');
+    }
+}
+
+function cadastrar(){
+    var usuario = document.getElementById('input_usuario').value
+
+    if(usuario.length < 6){
+        alert('Revise seu nome de usuário')
+    }else{
+        alert('Cadastrado com sucesso!')
+    }
+}
+
+function entrar(){
+    const email = input_email.value
+    const senha = input_senha.value
+
+    if(email == 'hfsystem@sptech.school' && senha == 'admin123'){
+        alert('Login realizado com sucesso')
+        window.location.href = "cadastro.html";
+    }else{
+        alert('Email ou senha incorretos')
     }
 }
