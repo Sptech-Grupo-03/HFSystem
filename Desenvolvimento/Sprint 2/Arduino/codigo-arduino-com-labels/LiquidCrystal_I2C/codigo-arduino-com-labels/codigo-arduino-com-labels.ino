ddbf74fa5dee6db98ce53e68a42623dfc54bae29
nicollyssousa <nicolly.sousa@sptech.school>
2024-10-22 21:41:22 -0300

    atualização do codigo arduino

--- Desenvolvimento/Sprint 2/Arduino/codigo-arduino-com-labels/LiquidCrystal_I2C/codigo-arduino-com-labels/codigo-arduino-com-labels.ino
+++ Desenvolvimento/Sprint 2/Arduino/codigo-arduino-com-labels/LiquidCrystal_I2C/codigo-arduino-com-labels/codigo-arduino-com-labels.ino
@@ -1,4 +1,5 @@
-// Inclui a biblioteca para o sensor HC-SR04
+//1° Bloco - Pinos do sensor
+//Inclui a biblioteca para o sensor HC-SR04
 #include "Ultrasonic.h"
 
 // Definições dos pinos de trigger e echo
@@ -8,18 +9,16 @@ const int PINO_ECHO = 13;
 // Cria uma instância do objeto HC_SR04 com os pinos definidos
 HC_SR04 sensor(PINO_TRIGGER, PINO_ECHO);
 
-// Função de inicialização
+// 2° Bloco - Configura a comunicação maquina x arduino
 void setup() {
   Serial.begin(9600);  // Inicializa a comunicação serial a 9600 bps
 }
 
-// Função principal de execução contínua
-void loop() {
-  // Exibe a distância no monitor serial
-  
+// 3° Bloco - Função principal de execução contínua
+// Exibe a distância captada pelo sensor
+void loop() {  
+
   Serial.println(sensor.distance());
-
-
   // Aguarda 1 segundo antes da próxima leitura
   delay(1000);
 }
