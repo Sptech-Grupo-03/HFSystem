a40f4b997dc64e608588294a6ef6c5d5e3a29028
Galactiana <ana.barrocal@sptech.school>
2024-11-07 23:09:04 -0300

    Atualização de script tabelas e DER

--- Desenvolvimento/Sprint 2/Banco de Dados/hfsystem.sql
+++ Desenvolvimento/Sprint 2/Banco de Dados/hfsystem.sql
@@ -2,113 +2,175 @@ create database HFSystem;
 
 use HFSystem;
 
-create table enderecoEmpresa(
+create table endereco(
 idEndereco int primary key auto_increment, 
-cep char(8),
-bairro varchar(45),
-rua varchar (50),
-cidade varchar (45), 
-uf char(2), 
-numero int
+tipo varchar(45),
+cep char (8),
+complemeto varchar(45),
+numero int,
+uf char(2),
+constraint checkTipo check (tipo in ('Fazenda', 'Empresa'))
+);
+
+create table empresa (
+idEmpresa int primary key auto_increment,
+razaoSocial varchar(100),
+nomeFantasia varchar(100),
+cnpj char(14),
+representanteLegal varchar(45),
+telefone char(11),
+email varchar(100),
+tipoEmpresa varchar(45),
+dtCadastro date,
+fkEndereco int,
+constraint fkEnderecoEmpresa foreign key (fkEndereco)
+references endereco(idEndereco)
 );
 
 create table fazenda(
-idEmpresa int primary key auto_increment, 
-razaoSocial varchar(45), 
-nomeFantasia varchar(45),
+codigoFazenda char(10) primary key, 
+razaoSocial varchar(100), 
+nomeFantasia varchar(100),
 cnpj char(14),
-representanteLegal varchar(100),
 telefone char(11), 
 email varchar(100),
 dataCadastro date,
-fkEndereco int, 
-constraint fkEnderecoEmpresa foreign key (fkEndereco)
-	references enderecoEmpresa (idEndereco)
+fkEmpresa int, 
+fkEndereco int,
+constraint fkFazendaEmpresa foreign key (fkEmpresa)
+	references Empresa(idEmpresa),
+constraint fkFazendaEndereco foreign key (fkEndereco)
+	references endereco(idEndereco)
 );
 
-create table funcionario(
-idFuncionario int primary key auto_increment,
+create table usuario(
+idUsuario int primary key auto_increment,
+username varchar(120),
 nome varchar(45), 
 telefone char(11), 
 email varchar(100), 
 senha char(10),
-cargo varchar(20),
-nivelAcesso varchar(25), 
-constraint checkNivelAcesso check (nivelAcesso in ('Administrador', 'Funcionario Comum')),
-fkEmpresaRegistro int, 
-constraint fkEmpresaFuncionario foreign key (fkEmpresaRegistro)
-	references fazenda (idEmpresa)
+fkFazenda int,
+constraint fkUsuarioFazenda foreign key (fkFazenda)
+	references fazenda(idFazenda)
 );
 
 create table reservatorio(
 idReservatorio int primary key auto_increment, 
-capacidade float not null, 
-tipo varchar(45) not null,
+raio float not null,
 altura float not null,
-largura float not null,
-diasAteReabastecimento int,
-fkEmpresa int,
- constraint fkEmpresaReservatorio foreign key (fkEmpresa)
-	references fazenda (idEmpresa) 
+fkFazenda int,
+ constraint fkFazendaReservatorio foreign key (fkFazenda)
+	references fazenda(idFazenda) 
 );
 
-create table hcsr04(
-idSensor int primary key auto_increment,
-nivelAgua float, 
-dataColeta date,
+CREATE TABLE historicoMedicao (
+idHistoricoMedicao int primary key,
+dtHrMedicao datetime,
+nivelAgua float,
+capacidadeCalculada float,
+fkReservatorio int,
+constraint fkReservatorioHistorico foreign key (fkReservatorio) 
+references reservatorio(idReservatorio)
+);
+
+create table clima (
+idClima int primary key auto_increment,
+probabilidadeDeChuva varchar(45),
+UmidadeDoAr float,
+temperaturaMinima float,
+temperaturaMaxima float,
+dtHrColeta datetime,
+fkFazenda char(10),
+constraint fkFazendaClima foreign key (fkFazenda)
+ references fazenda(idFazenda)
+ );
+
+create table aviso(
+idAviso int,
+aviso varchar(45),
+descricao varchar(45),
+dtHrEmissao date,
+fkReservatorio int,
+constraint checkAviso check (aviso in ('Risco', 'Alerta')),
+constraint fkReservatorioAviso foreign key (fkReservatorio) 
+references reservatorio(idReservatorio));       
+
+create table sensor(
+idColeta int primary key auto_increment,
+distanciaAgua float, 
+dtHrColeta datetime,
 fkReservatorio int,
 constraint fkSensorReservatorio foreign key (fkReservatorio)
 	references reservatorio (idReservatorio)
 );
 
-insert into enderecoEmpresa (cep, bairro, rua, cidade, uf, numero) values
-('01414001', 'Cerqueira César', 'Rua Haddock Lobo', 'São Paulo', 'SP', 595),
-('24130000', 'Centro', 'Praça do Conhecimento', 'São Gonçalo', 'RJ', 200),
-('01010000', 'Centro', 'Avenida São João', 'São Paulo', 'SP', 100),
-('01310000', 'Cerqueira César', 'Avenida Paulista', 'São Paulo', 'SP', 500),
-('76500000', 'Centro', 'Rua José de Alencar', 'Aparecida de Goiânia', 'GO', 250);
+insert into endereco (tipo, cep, complemento, numero, uf) values
+('Empresa', '01414001',  'Andar 10', 595,'SP'),
+('Empresa', '24130000', 'Andar 11', 200, 'RJ'),
+('Fazenda', '01010000', 'Sede', 100,'SP'),
+('Fazenda', '01310000', 'Sede', 500, 'SP'),
+('Empresa', '76500000', 'Bloco B', 250, 'GO');
 
-insert into fazenda (razaoSocial, nomeFantasia, cnpj, representanteLegal, telefone, email, dataCadastro, fkEndereco) values
-('JBS S/A', 'JBS', '02916265000160', 'Guilherme Perboyre Cavalcanti', '11987654321', 'imprensa@jbs.com.br', '2024-10-10', 1),
-('BRF S.A.', 'BRF', '02939930000110', 'Lorival Luz', '11987654322', 'contato@brf-br.com', '2024-10-12', 2),
-('Cargill Agrícola S/A', 'Cargill', '60584425000100', 'Fernando Pimenta', '31987654321', 'info@cargill.com.br', '2024-10-14', 3),
-('Bunge Brasil', 'Bunge', '60822150000140', 'Ricardo Josué', '21987654321', 'atendimento@bunge.com.br', '2024-10-13', 4),
-('SLC Agrícola S/A', 'SLC', '02810719000102', 'Aldo G. Meira', '31987654324', 'contato@slcagricola.com.br', '2024-10-15', 5);
+insert into fazenda (codigoFazenda, razaoSocial, nomeFantasia, cnpj, telefone, email, dataCadastro, fkEmpresa, fkEndereco) values
+( 'ADFEFSGR23','JBS S/A', 'JBS', '02916265000160',  '11987654321', 'imprensa@jbs.com.br', '2024-10-10', 1, 1),
+( '2ECC71FF11','BRF S.A.', 'BRF', '02939930000110',  '11987654322', 'contato@brf-br.com', '2024-10-12', 2, 2),
+('F1C40FAA23' ,'Cargill Agrícola S/A', 'Cargill', '60584425000100', '31987654321', 'info@cargill.com.br', '2024-10-14', 3, 3),
+('D35400BB44' ,'Bunge Brasil', 'Bunge', '60822150000140', '21987654321', 'atendimento@bunge.com.br', '2024-10-13', 4, 4),
+('8E44ADDD55' ,'SLC Agrícola S/A', 'SLC', '02810719000102', '31987654324', 'contato@slcagricola.com.br', '2024-10-15', 5, 5);
 
-insert into funcionario (nome, telefone, email, senha, cargo, nivelAcesso, fkEmpresaRegistro) values
-('Vinicius Gonçalves', '47632499830', 'vinicius@jbs.com.br', 'senha1234', 'Gerente', 'Administrador', 1),
-('Nicoly Souza', '47987654321', 'nicoly@brf.com.br', 'senhaNick1', 'Gerente', 'Administrador', 2),
-('Ana Barrocal', '48987654322', 'ana@cargill.com.br', 'senhaAna2', 'Supervisor', 'Funcionario Comum', 3),
-('Leonardo Sardinha', '49987654323', 'leo@bunge.com.br', 'senhaLeo3', 'Técnico', 'Funcionario Comum', 4),
-('Matheus Martinez', '47987654324', 'Math@slcagricola.com.br', 'senhaMath1', 'Analista', 'Funcionario Comum', 5);
+insert into usuario (username, nome, telefone, email, senha, fkFazenda) values
+('ViniGo', 'Vinicius Gonçalves', '47632499830', 'vinicius@jbs.com.br', 'senha1234', 1),
+('NickSo', 'Nicoly Souza', '47987654321', 'nicoly@brf.com.br', 'senhaNick1', 2),
+('AnnaBa', 'Ana Barrocal', '48987654322', 'ana@cargill.com.br', 'senhaAna2', 3),
+('LeonardSa', 'Leonardo Sardinha', '49987654323', 'leo@bunge.com.br', 'senhaLeo3', 4),
+('MathMart', 'Matheus Martinez', '47987654324', 'Math@slcagricola.com.br', 'senhaMath1',  5);
 
-insert into reservatorio (capacidade, tipo, altura, largura, diasAteReabastecimento, fkEmpresa) values
-(50000.0, 'Cilíndrico', 5.0, 3.0, 30, 1),
-(80000.0, 'Tanque', 6.0, 4.0, 45, 2),
-(70000.0, 'Cilíndrico', 5.0, 3.0, 40, 3),
-(100000.0, 'Tanque', 7.0, 5.0, 60, 4),
-(60000.0, 'Cilíndrico', 5.0, 3.5, 35, 5);
+insert into reservatorio (raio, altura, fkFazenda) values
+(2.5, 1, '2ECC71FF11'),
+(3.0, 2,'ADFEFSGR23' ),
+(2.5, 3, 'F1C40FAA23'),
+(3.0, 4, 'D35400BB44'),
+(2.5, 5, '8E44ADDD55');
 
-insert into hcsr04 (nivelAgua, dataColeta, fkReservatorio) values
+insert into clima (probabilidadeDeChuva, umidadeDoAr, temperaturaMinina, temperaturaMaxima, dtHrColeta, fkFazenda) values
+('2 Dias', '80', '18', '2024-10-18 07:00:00', '8E44ADDD55'),
+('4 Dias', '60', '28', '2024-10-18 07:00:00', 'D35400BB44'),
+('Sem chuvas', '70', '', '20', '2024-10-18 07:00:00', 'F1C40FAA23'),
+('7 Dias', '50', '30', '2024-10-18 07:00:00', 'ADFEFSGR23'),
+('1 dia', '52', '32', '2024-10-18 07:00:00', '2ECC71FF11');
+
+insert into sensor (distanciaAgua, dtHrColeta, fkReservatorio) values
 (1.5, '2024-10-18', 1),
 (2.0, '2024-10-19', 2),
 (1.0, '2024-10-20', 3),
 (2.0, '2024-10-21', 4),
 (1.0, '2024-10-22', 5);
 
-select * from hcsr04 join reservatorio on idReservatorio = fkReservatorio;
+INSERT INTO historicoMedicao (idHistoricoMedicao, dtHrMedicao, nivelAgua, capacidadeCalculada, fkReservatorio) VALUES
+(1, '2024-10-01 14:00:00', 7.5, 300.0, 1),
+(2, '2024-10-01 15:00:00', 8.0, 320.0, 2),
+(3, '2024-10-01 16:00:00', 7.0, 420.0, 3),
+(4, '2024-10-01 17:00:00', 7.5, 420.0, 4),
+(5, '2024-10-01 18:00:00', 8.0, 520.0, 5);
 
-select funcionario.nome as Funcionario,
+select * from sensor join reservatorio on idReservatorio = fkReservatorio;
+
+select usuario.nome as Funcionario,
 fazenda.razaoSocial as "Fazenda contratado"
-from funcionario
+from usuario
 join fazenda
-on funcionario.fkEmpresaRegistro = fazenda.idEmpresa;
+on usuario.fkfazenda = fazenda.idUsuario;
 
 select fazenda.razaoSocial as Fazenda,
-reservatorio.capacidade as CapacidadeReservatorio,
-reservatorio.tipo as TipoReservatorio,
+clima.temperaturaMaxima as TemperaturaMaxima,
+clima.umidadeDoAr as Umidade,
 reservatorio.altura as AlturaReservatorio,
-reservatorio.largura as LarguraReservatorio
+endereco.tipo as EnderecoTipo	
 from fazenda
 join reservatorio
-on fazenda.idEmpresa = reservatorio.fkEmpresa;
+on fazenda.codigoFazenda = reservartorio.fkFazenda
+join clima 
+on fazenda.codigoFazenda = clima.fkFazenda
+join endereco
+on fazenda.fkEndereco = endereco.idEndereco;
