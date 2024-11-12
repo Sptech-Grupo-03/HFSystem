485abda9ea186b801548d82fd9a3499d7f019805
matheusraffu <matheus.martinez@sptech.school>
2024-11-12 01:16:31 -0300

    AtualizacaoDash

--- Desenvolvimento/Sprint 3/Site/public/private/js/dash.js
+++ Desenvolvimento/Sprint 3/Site/public/private/js/dash.js
@@ -126,4 +126,7 @@ hamburgerIcon.addEventListener('click', () => {
     sidebar.classList.toggle('active');  // Ativa ou desativa a sidebar
     body.classList.toggle('sidebar-active');  // Aplica o efeito de desfoque no fundo
     hamburgerIcon.classList.toggle('open');  // Rotaciona o ícone do hamburger
+    document.addEventListener("DOMContentLoaded", function() {
+      HGWeather.initialize();
+    });
 });
