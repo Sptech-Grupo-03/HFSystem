d3b8365c4457ec2cec013b6d74df5bce28ea47a4
sardinh4 <leonardo_sardinha@outlook.com>
2024-11-20 09:58:36 -0300

    configurando a web-data-ac-ino

--- Desenvolvimento/Sprint 3/Arduino/arduino/main.js
+++ Desenvolvimento/Sprint 3/Arduino/arduino/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -20,9 +20,9 @@ const serial = async (
     let poolBancoDados = mysql.createPool(
         {
             host: 'localhost',
-            user: 'user_insert',
-            password: 'Hfsystemy#2024',
-            database: 'HF_System',
+            user: 'hf_system_insert',
+            password: 'Hfsystem#2024',
+            database: 'HFSystem',
             port: 3307
         }
     ).promise();
@@ -63,7 +63,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico) VALUES (?)',
+                'INSERT INTO sensor (distanciaAgua) VALUES (?)',
                 [sensorDigital]
             );
             console.log("valores inseridos no banco: " + sensorDigital);
